36df2aa53deb825fe53fd32aebf039be77691ba5
Miquel Jayson de Leeuw <mjleeuw@macc02cm14emd6w.localdomain>
2024-06-20 13:30:08 +0200

    Init Project

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -3,6 +3,11 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
+/*
+*   Miquel Jayson de Leeuw
+*   Convert numbers into Roman Numerals
+*/
+
 class MainKtTest {
 
     @Test
