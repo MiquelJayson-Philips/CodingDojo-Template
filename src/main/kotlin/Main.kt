645ad6bde5a100441612a9988391425aad1a0c43
MiquelJayson-Philips <MiquelJayson.de.leeuw@philips.com>
2024-06-21 12:34:51 +0200

    Final Algortihm

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,107 +1,31 @@
 package org.example
 
 enum class RomanNumeral(val char: String, val value: Int) {
-    ONE("I", 1),
-    FIVE("V", 5),
-    TEN("X", 10),
-    FIFTY("L", 50),
-    HUNDRED("C", 100),
-    FIVE_HUNDRED("D", 500),
-    THOUSAND("M", 1000),
-}
-
-enum class SpecialRomanNumeral(val char: String, val value: Int) {
-    FOUR("IV", 4),
-    NINE("IX", 9),
-    FORTY("XL", 40),
-    NINETY("XC", 90),
-    FOUR_HUNDRED("CD", 400),
-    NINE_HUNDRED("CM", 900),
-    NONE("", 0)
-}
-
-data class ConstructorData(
-    val output: String,
-    val remainder: Int
-)
-
-fun buildRomanNumeralStringFromNumber(number: Int, numeral: RomanNumeral): String {
-    val times = number.calculateAmountOfTimesItFits(numeral.value)
-    return numeral.char.repeat(times)
-}
-
-fun Int.calculateLeftover(amount: Int): Int {
-    return this % amount
-}
-
-fun Int.calculateAmountOfTimesItFits(amount: Int): Int {
-    return this / amount
-}
-
-fun constructRomanNumeral(
-    data: ConstructorData,
-    numeral: RomanNumeral,
-    specialNumeral: SpecialRomanNumeral
-): ConstructorData {
-    var builderString = data.output
-    var remaining = data.remainder
-
-    builderString += buildRomanNumeralStringFromNumber(remaining, numeral)
-    remaining = remaining.calculateLeftover(numeral.value)
-
-    if (remaining >= specialNumeral.value) {
-        builderString.dropLast(1)
-        builderString += specialNumeral.char
-        remaining -= specialNumeral.value
-    }
-
-    return ConstructorData(builderString, remaining)
+    M("M", 1000),
+    CM("CM", 900),
+    D("D", 500),
+    CD("CD", 400),
+    C("C", 100),
+    XC("XC", 90),
+    L("L", 50),
+    XL("XL", 40),
+    X("X", 10),
+    IX("IX", 9),
+    V("V", 5),
+    IV("IV", 4),
+    I("I", 1),
 }
 
 fun Int.romanNumeral(): String {
-    var result = ConstructorData("", this)
+    var remainder = this
+    var output = ""
 
-    result = constructRomanNumeral(
-        data = result,
-        numeral = RomanNumeral.THOUSAND,
-        specialNumeral = SpecialRomanNumeral.NINE_HUNDRED
-    )
+    RomanNumeral.entries.forEach { numeral ->
+        while(remainder >= numeral.value){
+            output += numeral.char
+            remainder -= numeral.value
+        }
+    }
 
-    result = constructRomanNumeral(
-        data = result,
-        numeral = RomanNumeral.FIVE_HUNDRED,
-        specialNumeral = SpecialRomanNumeral.FOUR_HUNDRED
-    )
-
-    result = constructRomanNumeral(
-        data = result,
-        numeral = RomanNumeral.HUNDRED,
-        specialNumeral = SpecialRomanNumeral.NINETY
-    )
-
-    result = constructRomanNumeral(
-        data = result,
-        numeral = RomanNumeral.FIFTY,
-        specialNumeral = SpecialRomanNumeral.FORTY
-    )
-
-    result = constructRomanNumeral(
-        data = result,
-        numeral = RomanNumeral.TEN,
-        specialNumeral = SpecialRomanNumeral.NINE
-    )
-
-    result = constructRomanNumeral(
-        data = result,
-        numeral = RomanNumeral.FIVE,
-        specialNumeral = SpecialRomanNumeral.FOUR
-    )
-
-    result = constructRomanNumeral(
-        data = result,
-        numeral = RomanNumeral.ONE,
-        specialNumeral = SpecialRomanNumeral.NONE
-    )
-
-    return result.output
+    return output
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -9,37 +9,6 @@ import org.junit.jupiter.api.Test
 
 class MainKtTest {
 
-    /* Utils */
-
-    @Test
-    fun calculateLeftoverOfAmount() {
-        assertEquals(50, 150.calculateLeftover(100))
-    }
-
-    @Test
-    fun calculateHowManyTimesANumberFitsInCertainAmount() {
-        assertEquals(1, 150.calculateAmountOfTimesItFits(100))
-    }
-
-    @Test
-    fun buildRepeatingRomanNumeralStringFromNumber() {
-        assertEquals("M", buildRomanNumeralStringFromNumber(1000, RomanNumeral.THOUSAND))
-    }
-
-    @Test
-    fun constructRomanNumeralAndReturnRemainder() {
-        val result = constructRomanNumeral(
-            data = ConstructorData("", 500),
-            numeral = RomanNumeral.FIVE_HUNDRED,
-            specialNumeral = SpecialRomanNumeral.FOUR_HUNDRED
-        )
-
-        assertEquals("D", result.output)
-    }
-
-
-    /* Test Cases */
-
     @Test
     fun doesRomanNumeralAcceptAllPossibilities() {
         assertEquals("MDCLXVI", 1666.romanNumeral())
